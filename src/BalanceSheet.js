30687edd6dcd57d65bc79256cdcb98451999a970
Mikhail-Vilms <mikhail.vilms@gmail.com>
2022-07-16 23:10:45 -0500

    feat: consume and render raw json

--- src/BalanceSheet.js
+++ src/BalanceSheet.js
@@ -1,10 +1,38 @@
 import React, {Component} from "react";
+import {variables} from './Variables.js';
 
 export class BalanceSheet extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            consumedJson: null
+        }
+    }
+
+    // https://www.pluralsight.com/guides/fetch-data-from-a-json-file-in-a-react-app
+    fetchBalanceSheetData(){
+        fetch(variables.API_URL)
+            .then(response => {
+                return response.json();
+            })
+            .then(myJson => {
+                console.log("*** myJson *** : " + myJson);
+                this.setState({consumedJson:myJson});
+            });
+    }
+
+    componentDidMount(){
+        this.fetchBalanceSheetData();
+    }
+
     render(){
+        console.log("render ===  ==== " + JSON.stringify(this.state.consumedJson));
+        
         return (
             <div>
-                <h3>This is page to display Balance Sheet Statement information</h3>
+                qwdqwd
+                {JSON.stringify(this.state.consumedJson)}
+                {/* {this.state.departments} */}
             </div>
         )
     }
---
+++ src/Variables.js
@@ -0,0 +1,4 @@
+export const variables={
+    // API_URL: "https://lyropdpvy6.execute-api.us-west-2.amazonaws.com/dev/api/financial-data/CIK0000050863/BalanceSheet/Assets"
+    API_URL: "https://localhost:44306/financial-data/CIK0000050863/BalanceSheet/Assets"
+}
